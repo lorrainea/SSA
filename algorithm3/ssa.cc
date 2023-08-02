8b64afec81bfef3a257b2a2037b7b192c7a52fbd
Solon P Pissis <solonas13@gmail.com>
2023-08-02 08:50:01 +0200

    fix

--- algorithm3/ssa.cc
+++ algorithm3/ssa.cc
@@ -357,6 +357,7 @@ int main(int argc, char **argv)
 	cout<<"Number of suffixes b = " << b << endl;
 	
 	uint64_t s = 2*b;
+	if ( s > text_size ) s = text_size;
 	uint64_t fp_len = text_size / s;
 	if ( (text_size - fp_len * s) > text_size/s ) 
 		s = text_size/fp_len; 
