358de6d955bebcea8422137d538f82e60aad2431
gloukides <grigorios.loukides@kcl.ac.uk>
2023-08-04 16:27:49 +0100

    tester with break

--- tester/tester.py
+++ tester/tester.py
@@ -3,9 +3,9 @@ import string
 import sys
 import os
 
-seq_len = 20 #can be changed
-num_suffixes = 10 #can be changed
-runs = 1 #can be changed
+seq_len = 2000000 #can be changed
+num_suffixes = 500000 #can be changed
+runs = 1000 #can be changed
 
 for i in range(0, runs):
 	letters = string.ascii_lowercase
@@ -27,7 +27,7 @@ for i in range(0, runs):
 	print("RUNNING GREG")
 	run_greg = r'../greg/./ssa text suffixes ../greg/out'
 	os.system(run_greg)
-	
+           
 	print("RUNNING ALGORITHM3")
 	run_alg3 = r'../algorithm3/./ssa text suffixes ../algorithm3/out'
 	os.system(run_alg3)
@@ -43,26 +43,30 @@ for i in range(0, runs):
 	
 	print("COMPARING GREG AND SDSL")
 	run_diff_ssa = r'diff out_accurate.ssa ../greg/out.ssa'	
-	os.system(run_diff_ssa)
+	if os.system(run_diff_ssa)==1:
+            break;
 	
-	run_diff_lcp = r'diff out_accurate.lcp ../greg/out.lcp'	
-	os.system(run_diff_lcp)
-	
+	run_diff_lcp = r'diff out_accurate.lcp ../greg/out.lcp '	
+	if os.system(run_diff_lcp)==1:
+	    break;
 	
 	print("COMPARING ALGORITHM3 AND SDSL")
 	run_diff_ssa_alg3 = r'diff out_accurate.ssa ../algorithm3/out.ssa'	
-	os.system(run_diff_ssa_alg3)
-	
+	if os.system(run_diff_ssa_alg3)==1:
+            break;
+
 	run_diff_lcp_alg3 = r'diff out_accurate.lcp ../algorithm3/out.lcp'	
-	os.system(run_diff_lcp_alg3)
-	
+	if os.system(run_diff_lcp_alg3)==1:
+	    break;
 	
 	print("COMPARING GREG AND RK-LCE")
 	run_diff_ssa_rk_lce = r'diff ../greg/out.ssa ../rk-lce/out_rk_lce.ssa'	
-	os.system(run_diff_ssa_rk_lce)
+	if os.system(run_diff_ssa_rk_lce)==1:
+            break;
 	
 	run_diff_lcp_rk_lce = r'diff ../greg/out.lcp ../rk-lce/out_rk_lce.lcp'	
-	os.system(run_diff_lcp_rk_lce)
+	if os.system(run_diff_lcp_rk_lce)==1:
+            break;
 	
 	
 	#run_lorraine = r'.././ssa text suffixes ../out'
