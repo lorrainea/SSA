6ea6dcd0767600d8eabc2865c2d60e9536d0a215
Solon P Pissis <solonas13@gmail.com>
2023-08-04 16:16:07 +0200

    fix

--- random-suf/random.cc
+++ random-suf/random.cc
@@ -1,50 +1,57 @@
+#include <random>
 #include <iostream>
 #include <unordered_set>
 #include <algorithm>
 #include <set>
+
 using namespace std;
-  
+
 int main(int argc, char **argv)
 {
-	if( argc < 3 )
+        if( argc < 3 )
         {
                 cout<<"Check arguments!\n";
                 cout<<"./random <n> <#suffixes>\n";
                 exit(-1);
-    	}
-    	uint64_t N = (uint64_t) atoi(argv[1]);
-    	uint64_t suf = (uint64_t) atoi(argv[2]);
-	
-	if (suf > N )
+        }
+        uint64_t N = (uint64_t) atoi(argv[1]);
+        uint64_t suf = (uint64_t) atoi(argv[2]);
+
+        if (suf > N )
         {
                 cout<<"Check arguments!\n";
                 cout<<"n must be greater than #suffixes!!!\n";
                 exit(-1);
-    	}
+        }
 
-	unordered_set<uint64_t> set;
-    	uint64_t * S;
-    	S = (uint64_t *)calloc(suf, sizeof(uint64_t));
-    	
-	for (uint64_t i = 0; i < suf; i++)	
-	{
-		while(1)
-		{
-			uint64_t key = rand() % N;
-			if ( set.find(key) == set.end() )	
-			{
-				S[i] = key;
-				set.insert(key);
-				break;
-			}
-		}
-	}
+        unordered_set<uint64_t> set;
+        uint64_t * S;
+        S = (uint64_t *)calloc(suf, sizeof(uint64_t));
 
-	std::sort(S, S + suf);
-    	
-	for (uint64_t i = 0; i < suf; i++)	cout<<S[i]<<endl;
-  
-    	free(S);
+        std::random_device rd;
+        std::mt19937_64 gen(rd());
+        std::uniform_int_distribution<uint64_t> dis(0,N-1);
 
-    	return 0;
+        for (uint64_t i = 0; i < suf; i++)
+        {
+                while(1)
+                {
+                        uint64_t key = dis(gen);
+                        if ( set.find(key) == set.end() )
+                        {
+                                S[i] = key;
+                                set.insert(key);
+                                break;
+                        }
+                }
+        }
+
+        std::sort(S, S + suf);
+
+        for (uint64_t i = 0; i < suf; i++)      cout<<S[i]<<endl;
+
+        free(S);
+
+        return 0;
 }
+
