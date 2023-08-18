91a278cebff1cfce1c1ffb132f925dd3b749b32b
Solon P Pissis <solonas13@gmail.com>
2023-08-18 08:05:46 +0200

    header

--- PA/ssa.cc
+++ PA/ssa.cc
@@ -1,3 +1,22 @@
+/**
+    Sparse Suffix and LCP Array: Simple, Direct, Small, and Fast
+    Copyright (C) 2021 Lorraine A. K. Ayad, Grigorios Loukides, 
+    Solon P. Pissis and Hilde Verbeek
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 #include<iostream>
 #include <cstring>
 #include <string>
